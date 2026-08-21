27baf9b3846b7f729d5ea0f3711458c1d9f9854c
Saphal Kafle <saphalkafle77@gmail.com>
2026-08-21 22:31:26 +0545

    login ui

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import streamlit as st
 
-st.set_page_config(page_title="Real-time Gym Trainer")
-st.title("Real-time Gym Trainer")
-st.write("Setup done! Now you can start your workout session.")
+from services.auth.login import render_login_page
+
+def main():
+    st.set_page_config(
+       page_icon="bodybuilder",
+       page_title="AI Real-time Gym Trainer",
+       initial_sidebar_state="expanded",
+       layout="centered"
+    )
+
+    if not render_login_page():
+       return
+
+if __name__ == "__main__":
+    main()
