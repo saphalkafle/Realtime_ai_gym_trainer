dbcbedf45a8df565db75b753f6329b7a268d2f7a
Saphal Kafle <saphalkafle77@gmail.com>
2026-08-23 20:49:50 +0545

    Exercise in sidebar

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
-
+from services.state.session_default import initial_session_default
 from services.auth.login import render_login_page
+from services.config.workout_names import Exercise_options
 
 def main():
     st.set_page_config(
@@ -12,6 +13,26 @@ def main():
 
     if not render_login_page():
        return
-    st.write("Welcome to your AI gym trainer!!")
+
+    initial_session_default()
+
+    workout_started = st.session_state.get("workout_started",False)
+    with st.sidebar:
+        st.title("Personal Home AI Trainer")
+
+        if st.session_state.username:
+            st.caption(f"👤 Logged in as {st.session_state.username}")
+
+        st.divider()
+
+        st.subheader("Workout Plan")
+
+        if not workout_started:
+         
+         st.selectbox('Exercises',options=Exercise_options,key='exercise_name')
+        
+        
+
+    
 if __name__ == "__main__":
     main()
