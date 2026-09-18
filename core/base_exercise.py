cddcab9a23e093c8a8b333309e3ce178cd6fe948
Saphal Kafle <saphalkafle77@gmail.com>
2026-09-18 18:01:15 +0545

    base exercise logic

--- core/base_exercise.py
+++ core/base_exercise.py
@@ -1,1 +1,34 @@
 from abc import ABC, abstractmethod
+import math
+class BaseExercise(ABC):
+    def __init__(self):
+        self.reps = 0
+        self.stage = None #position 
+
+    def calculate_angle(self,a,b,c):
+        ax,ay = a[0] - b[0], a[1] - b[1]
+        cx,cy = c[0] - b[0], c[1] - b[1]
+
+        dot_product = (ax*cx) + (ay*cy)
+
+        magnitude_a = math.sqrt(ax ** 2 + ay ** 2)
+        magnitude_c = math.sqrt(cx ** 2 + cy ** 2)
+
+        if magnitude_a * magnitude_c == 0:
+            return 0.0
+
+        cosine_angle = max(-1.0 ,min(1.0,dot_product / (magnitude_a * magnitude_c)))
+
+        return math.degrees(math.acos(cosine_angle))
+
+    def get_point(self,landmarks,idx):
+        p = landmarks[idx]
+        return(p.x,p.y)
+
+    @abstractmethod
+    def process(self,landmarks):
+        pass
+
+    @abstractmethod
+    def reset(self):
+        pass
